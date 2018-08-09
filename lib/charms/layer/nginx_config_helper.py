d4371fdf3a6a6ea104233ce9c7a94224efb181cc
sborny <sander.borny@ugent.be>
2018-08-09 15:55:34 +0200

    Restructured lib

--- lib/charms/layer/nginx_config_helper.py
+++ lib/charms/layer/nginx_config_helper.py
@@ -1,5 +1,6 @@
 import os
 import inspect
+from enum import Enum
 from pathlib import Path
 from shutil import copyfile
 from subprocess import CalledProcessError, run
@@ -14,47 +15,34 @@ class NginxConfigError(Exception):
     pass
 
 
-# CAUTION ! You can only use this class after the 'nginx-config.installed' flag is set !
-class NginxConfig(object):
+class NginxModule(Enum):
+    """Enum of the valid NGINX modules.
+
+    Valid options are:
+     * 'NginxModule.HTTP'
+     * 'NginxModule.STREAM'
+    """
+    # note: values should correspond to Class types in the python-nginx lib
+    HTTP = 'Http'
+    STREAM = 'Stream'
+
+
+# CAUTION ! You can only use this class after 
+# the 'nginx-config.installed' flag is set !
+class NginxBase(object):
 
     def __init__(self):
         kv = unitdata.kv()
-        self._layer = _find_calling_layer()
-        if not self.layer:
-            raise NginxConfigError("Could not initialize NginxConfig, \
-                                     no layer name found.")
-        self._juju_app_name = kv.get('juju_app_name')        
         self.config_path = kv.get('nginx_config_path')
+        self._juju_app_name = kv.get('juju_app_name')
         self._juju_config_path = kv.get('nginx_juju_path')
         self._backup_path = kv.get('nginx_backups')
         self._streams_enabled_path = kv.get('nginx_streams_enabled')
-        self._http_enabled_path = kv.get('nginx_http_enabled')       
-        self._nginx_config = self.config_as_object()    
-        self.ensure_unit_dir_exists()
-
-    @property
-    def layer(self):
-        return self._layer
+        self._http_enabled_path = kv.get('nginx_http_enabled')
 
     @property
     def juju_app_name(self):
         return self._juju_app_name
-
-    @property
-    def http_available_path(self):
-        path = os.path.join(self._juju_config_path,
-                            self._juju_app_name, 
-                            self._layer, 
-                            "sites-available")
-        return path
-
-    @property
-    def streams_available_path(self):
-        path = os.path.join(self._juju_config_path,
-                            self._juju_app_name, 
-                            self._layer, 
-                            "streams-available")
-        return path
 
     @property
     def http_enabled_path(self):
@@ -67,69 +55,6 @@ class NginxConfig(object):
     @property
     def backup_path(self):
         return self._backup_path
-
-    @property
-    def nginx_config(self):
-        return self._nginx_config
-
-    def config_as_dict(self):
-        """Returns the NGINX main config file as a dict.
-        """
-        return self._nginx_config.as_dict
-
-    def config_as_object(self):
-        """Returns the NGINX main config file as a nginx.Conf object.
-        """
-        return nginx.loadf(self.config_path)
-
-    def get_includes(self, module='Http'):
-        """Return all includes values from a module.
-
-        Args:
-            module (str): Should be one of the following options ['Http' || 'Stream'].   # TODO Enum?
-
-        Returns:
-            list: A list containing all found include values.
-        """
-        cfg = self._nginx_config
-        m = self.load_module(cfg, module)       
-        includes = []
-        for m_cfg in m.as_dict:
-            if 'include' in m_cfg:
-                includes.append(m_cfg['include'])
-        return includes
-
-    def add_include(self, include, module='Http'):
-        """Adds an include value to the module.
-
-        Args:
-            include (str): Include string.
-            module (str): Should be one of the following options ['Http' || 'Stream'].
-        """
-        cfg = self._nginx_config
-        m = self.load_module(cfg, module)
-        # prevent duplicates
-        includes = self.get_includes(module)
-        if include not in includes:
-            m.add(nginx.Key('include', include))
-        return self
-
-    def load_module(self, nginx_cfg, module='Http'):
-        """Return a module from the main NGINX config file.
-
-        Args:
-            nginx_cfg (nginx.Conf): NGINX object
-            module (str): Should be one of the following options ['Http' || 'Stream'].
-
-        Returns:
-            nginx.Http || nginx.Stream
-        """
-        try:
-            m = nginx_cfg.filter(module)[0] # There should only be one Http / Stream block
-            return m
-        except IndexError:
-            err_msg = 'Could not find module {} in nginx.conf'.format(module)
-            raise NginxConfigError(err_msg)
 
     def validate_nginx(self):
         """Validates the NGINX configuration. 
@@ -157,27 +82,104 @@ class NginxConfig(object):
             log(e)
             status_set('blocked', err_msg)
             raise NginxConfigError(err_msg)
-        return self    
+        return self  
 
-    def add_module(self, module): # Should module default to something?
+
+class NginxMainConfig(NginxBase):
+
+    def __init__(self):
+        NginxBase.__init__(self)
+        self._nginx_config = self.config_as_object()
+
+    @property
+    def nginx_config(self):
+        return self._nginx_config     
+
+    def config_as_dict(self):
+        """Returns the NGINX main config file as a dict.
+        """
+        return self._nginx_config.as_dict
+
+    def config_as_object(self):
+        """Returns the NGINX main config file as a nginx.Conf object.
+        """
+        return nginx.loadf(self.config_path)
+
+    def add_include(self, include, nginx_module):
+        """Adds an include value to the module.
+
+        Args:
+            include (str): Include string.
+            nginx_module (NginxModule): NGINX module.
+        """
+        cfg = self._nginx_config
+        m = self.load_module(cfg, nginx_module)
+        # prevent duplicates
+        includes = self.get_includes(nginx_module)
+        if include not in includes:
+            m.add(nginx.Key('include', include))
+        return self
+
+    def get_includes(self, nginx_module):
+        """Return all includes values from a NGINX module.
+
+        Args:
+            nginx_module (NginxModule): NGINX module.
+
+        Returns:
+            list: A list containing all found include values.
+        """
+        cfg = self._nginx_config
+        m = self.load_module(cfg, nginx_module)  
+        includes = []
+        for m_cfg in m.as_dict:
+            if 'include' in m_cfg:
+                includes.append(m_cfg['include'])
+        return includes    
+
+    def add_module(self, nginx_module): # Should module default to something?
         """Add a Http or Stream module to the NGINX config. If the module already
         exists, do nothing.
 
         Args:
-            module (str): Should be one of the following options ['Http' || 'Stream'].
+            nginx_module (NginxModule): NGINX module.
         """
-        if module != "Http" and module != "Stream":
+        if (nginx_module != NginxModule.HTTP 
+                and nginx_module != NginxModule.STREAM):
             raise NginxConfigError("Module should be either 'Http' or 'Stream'.")
         cfg = self._nginx_config
         try:
-            self.load_module(cfg, module)
+            self.load_module(cfg, nginx_module)
         except NginxConfigError: # Module does not exist yet
-            if module == "Http":
+            if nginx_module == NginxModule.HTTP:
                 cfg.add(nginx.Http())
             else:
                 cfg.add(nginx.Stream())
         return self
 
+    def load_module(self, nginx_cfg, nginx_module):
+        """Return a module from the main NGINX config file.
+
+        Args:
+            nginx_cfg (nginx.Conf): NGINX object
+            nginx_module (NginxModule): NGINX module.
+
+        Returns:
+            nginx.Http || nginx.Stream
+        """
+        try:
+            m = nginx_cfg.filter(nginx_module.value)[0] # There should only be one Http / Stream block
+            return m
+        except IndexError:
+            err_msg = 'Could not find module {} in nginx.conf' \
+                        .format(nginx_module.value)
+            raise NginxConfigError(err_msg)    
+    
+    def write_nginx_config(self):
+        """Writes the _nginx_config object to the nginx.conf file.
+        """
+        nginx.dumpf(self._nginx_config, self.config_path)
+        return self
 
     def backup_nginx_config(self, dst=None):
         """Creates a copy of the nginx.conf file to the destination. 
@@ -198,69 +200,141 @@ class NginxConfig(object):
             raise NginxConfigError(err_msg)
         return self
 
-    def write_nginx_config(self):
-        """Writes the _nginx_config object to the nginx.conf file.
-        """
-        nginx.dumpf(self._nginx_config, self.config_path)
-        return self
 
-    def write_http_config(self, http_config, file_name):  # Enum?
-        """Writes the HTTP config to the sites-available dir.
-        """
-        path = os.path.join(self.http_available_path, file_name) 
-        with open(path, "w") as f:
-            f.write(http_config)
-        return self
+class NginxConfig(NginxBase):
 
-    def write_stream_config(self, stream_config, file_name):    # Enum?
-        """Writes the stream config to the streams-available dir.
-        """
-        path = os.path.join(self.streams_available_path, file_name) 
-        with open(path, "w") as f:
-            f.write(stream_config)
-        return self
+    def __init__(self):
+        NginxBase.__init__(self)
+        self._layer = _find_calling_layer()
+        if not self.layer:
+            raise NginxConfigError("Could not initialize NginxConfig, \
+                                     no layer name found.")        
+        self._ensure_unit_dir_exists()
 
-    def enable_all_http(self):    # Enum?
-        """Create symb links in sites-enabled to all files in sites-available.
-        """
-        for f in os.listdir(self.http_available_path):
-            os.symlink(os.path.join(self.http_available_path, f),
-                    os.path.join(self.http_enabled_path, f))
-        return self
+    @property
+    def layer(self):
+        return self._layer
 
-    def enable_all_stream(self):    # Enum?
-        """Create symb links in sites-enabled to all files in sites-available.
-        """
-        for f in os.listdir(self.streams_available_path):
-            os.symlink(os.path.join(self.streams_available_path, f),
-                    os.path.join(self.streams_enabled_path, f))
-        return self
-
-    def delete_all_http(self):    # Enum?
-        """Delete all symb links and configs from sites-available.
-        """
-        for f in os.listdir(self.http_available_path):
-            if os.path.exists(os.path.join(self.http_enabled_path, f)):
-                os.unlink(os.path.join(self.http_enabled_path, f))
-            os.remove(os.path.join(self.http_available_path, f))
-        return self
-    
-    def delete_all_streams(self):    # Enum?
-        """Delete all symb links and configs from streams-available.
-        """
-        for f in os.listdir(self.streams_available_path):
-            if os.path.exists(os.path.join(self.streams_enabled_path, f)):
-                os.unlink(os.path.join(self.streams_enabled_path, f))
-            os.remove(os.path.join(self.streams_available_path, f))
-        return self
-
-    def ensure_unit_dir_exists(self): # Find better name
+    @property
+    def http_available_path(self):
         path = os.path.join(self._juju_config_path,
                             self._juju_app_name, 
-                            self._layer)
-        if not os.path.exists(path):
-            os.makedirs(os.path.join(path, "sites-available"), exist_ok=True)
-            os.makedirs(os.path.join(path, "streams-available"), exist_ok=True)
+                            self._layer, 
+                            "sites-available")
+        return path
+
+    @property
+    def streams_available_path(self):
+        path = os.path.join(self._juju_config_path,
+                            self._juju_app_name, 
+                            self._layer, 
+                            "streams-available")
+        return path  
+
+    def write_config(self, nginx_module, config, filename, subdir=None):   # TODO catch filename collision
+        """Writes the config to the nginx_module available dir.
+
+        Args:
+            nginx_module (NginxModule): NGINX module. 
+            config (str): NGINX config to be written.
+            filename (str): Name of the config file.
+            subdir (str): If specified, the value of subdir will be appended
+                to the available_path variable. Use this if you want to 
+                write the config to a self made directory.
+        """
+        available_path = self._available_path_nginx_module(nginx_module)
+
+        if subdir:
+            available_path += '/' + subdir.lstrip('/')
+
+        config_path = os.path.join(available_path, filename)
+        with open(config_path, 'w') as f:
+            f.write(config)
+        return self
+
+    def enable_all_config(self, nginx_module, subdir=None):
+        """Creates symb links in the nginx_module enabled dir for all files
+        in the available directory.
+
+        Args:
+            nginx_module (NginxModule): NGINX module. 
+            subdir (str): If specified, the value of subdir will be appended
+                to the available_path variable. Use this if you wrote configs
+                to a seperate directory.    
+        """    
+        available_path = self._available_path_nginx_module(nginx_module)
+        enabled_path = self._enabled_path_nginx_module(nginx_module)
+
+        if subdir:
+            available_path += '/' + subdir.lstrip('/')
+
+        for f in os.listdir(available_path):
+            os.symlink(os.path.join(available_path, f),
+                    os.path.join(enabled_path, f))
+        return self
+
+    def delete_all_config(self, nginx_module, subdir=None):
+        """Delete all symb links and configs from the nginx_module.
+
+        Args:
+            nginx_module (NginxModule): NGINX module.
+            subdir (str): If specified, the value of subdir will be appended
+                to the available_path variable. Use this if you don't want to
+                delete all configs in the available directory but instead 
+                a self made subdir.
+        """
+        available_path = self._available_path_nginx_module(nginx_module)
+        enabled_path = self._enabled_path_nginx_module(nginx_module)  
+        
+        if subdir:
+            available_path += '/' + subdir.lstrip('/')
+
+        for f in os.listdir(available_path):
+            if os.path.exists(os.path.join(enabled_path, f)):
+                os.unlink(os.path.join(enabled_path, f))
+            os.remove(os.path.join(available_path, f))
+        return self
+
+    def _ensure_unit_dir_exists(self): # Find better name
+        """Creates two paths for the calling layer, one for http 
+        configs and the other for stream configs.
+        """
+        path = os.path.join(self._juju_config_path,
+                                self._juju_app_name, 
+                                self._layer)
+        try:            
+            if not os.path.exists(path):
+                os.makedirs(os.path.join(path, "sites-available"), exist_ok=True)
+                os.makedirs(os.path.join(path, "streams-available"), exist_ok=True)
+        except OSError as e:
+            log(e)
+            raise NginxConfigError("Could not create path ({})".format(path))
+    
+    def _available_path_nginx_module(self, nginx_module):
+        """Returns the path to the available dir for the nginx_module.
+
+        Args:
+            nginx_module (NginxModule): NGINX module.
+        """
+        if nginx_module == NginxModule.HTTP:
+            return self.http_available_path
+        elif nginx_module == NginxModule.STREAM:
+            return self.streams_available_path
+        else:
+            raise NginxConfigError("Invalid NginxModule found.")
+    
+    def _enabled_path_nginx_module(self, nginx_module):
+        """Returns the path to the enabled dir for the nginx_module.
+
+        Args:
+            nginx_module (NginxModule): NGINX module.
+        """
+        if nginx_module == NginxModule.HTTP:
+            return self.http_enabled_path
+        elif nginx_module == NginxModule.STREAM:
+            return self.streams_enabled_path
+        else:
+            raise NginxConfigError("Invalid NginxModule found.")
         
 
 def _find_calling_layer(): # Class Method or not?
@@ -273,7 +347,3 @@ def _find_calling_layer(): # Class Method or not?
             continue
         return layer_name
     return None
-
-
-def what_layer_am_i(): # For Debugging purposes
-        return _find_calling_layer()
--- reactive/nginx_config.py
+++ reactive/nginx_config.py
@@ -2,7 +2,11 @@ import os
 from charms.reactive import when, when_not, set_flag
 from charmhelpers.core.hookenv import log, status_set
 from charmhelpers.core import unitdata
-from charms.layer.nginx_config_helper import NginxConfig, NginxConfigError
+from charms.layer.nginx_config_helper import (
+    NginxMainConfig, 
+    NginxConfigError, 
+    NginxModule,
+)
 
 @when('nginx.available')
 @when_not('nginx-config.installed')
@@ -16,23 +20,15 @@ def install_nginx_config():
         return
 
     juju_app_name = os.environ['JUJU_UNIT_NAME'].replace('/', '-')
-    # nginx_http_available = '{}/{}/sites-available' \
-    #                            .format(juju_config_path, juju_app_name)
-    # nginx_streams_available = '{}/{}/streams-available' \
-    #                            .format(juju_config_path, juju_app_name)
     nginx_streams_enabled = '/etc/nginx/streams-enabled'
     nginx_http_enabled = '/etc/nginx/sites-enabled'
     nginx_backups = '{}/backups'.format(juju_config_path)
 
     os.makedirs(nginx_backups, exist_ok=True)
     os.makedirs(nginx_streams_enabled, exist_ok=True)
-    # os.makedirs(nginx_http_available, exist_ok=True)
-    # os.makedirs(nginx_streams_available, exist_ok=True)
 
     unitdata.kv().set('juju_app_name', juju_app_name)
-    #unitdata.kv().set('nginx_http_available', nginx_http_available)
     unitdata.kv().set('nginx_http_enabled', nginx_http_enabled)
-    #unitdata.kv().set('nginx_streams_available', nginx_streams_available)
     unitdata.kv().set('nginx_streams_enabled', nginx_streams_enabled)
     unitdata.kv().set('nginx_backups', nginx_backups)
     unitdata.kv().set('nginx_config_path', config_path)
@@ -40,12 +36,12 @@ def install_nginx_config():
 
     # Ensure http / stream module exists and includes the right dirs
     try:
-        nginxcfg = NginxConfig()
+        nginxcfg = NginxMainConfig()
         nginxcfg \
-            .add_module('Http') \
-            .add_module('Stream') \
-            .add_include('/etc/nginx/sites-enabled/*', 'Http') \
-            .add_include('{}/*'.format(nginx_streams_enabled), 'Stream') \
+            .add_module(NginxModule.HTTP) \
+            .add_module(NginxModule.STREAM) \
+            .add_include('/etc/nginx/sites-enabled/*', NginxModule.HTTP) \
+            .add_include('{}/*'.format(nginx_streams_enabled), NginxModule.STREAM) \
             .write_nginx_config() \
             .validate_nginx() \
             .reload_nginx()
